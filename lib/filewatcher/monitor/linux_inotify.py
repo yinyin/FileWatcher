bf1cfec6e8183016bc22c318d08687ea3d929d11
Yinyin Liao <yinyinl@gmail.com>
2012-06-17 11:48:05 +0800

    complete inotify monitor

--- lib/filewatcher/monitor/linux_inotify.py
+++ lib/filewatcher/monitor/linux_inotify.py
@@ -7,6 +7,15 @@ from filewatcher import componentprop
 from filewatcher import filewatchconfig
 
 
+def _get_relpath(path, start):
+	relpath = os.path.relpath(path, start)
+	if '.' == relpath:
+		return ''
+	else:
+		return relpath
+# ### def _get_relpath
+
+
 _cached_module_prop_instance = componentprop.MonitorProp('inotify')
 def get_module_prop():
 	""" 取得監視器各項特性/屬性
@@ -37,6 +46,37 @@ def set_ignorance_checker(checker):
 # ### def set_ignorance_checker
 
 
+class _ExcludeFilter:
+	def __init__(self, target_directory, recursive_watch=False):
+		self.target_directory = target_directory
+		self.recursive_watch = recursive_watch
+	# ### def __init__
+	
+	def do_exclude_filting(self, filepath):
+		filepath = os.path.abspath(filepath)
+		if os.path.isdir(filepath):
+			path = filepath
+			name = None
+		else:
+			path, name, = os.path.split(filepath)
+
+		if (False == self.recursive_watch) and (path != self.target_directory):
+			return True
+
+		if _ignorance_checker is not None:
+			relpath = _get_relpath(path, self.target_directory)
+			r = _ignorance_checker(relpath, name)
+			if r:
+				return True
+
+		return False
+	# ### def do_exclude_filting
+
+	def __call__(self, path):
+		return self.do_exclude_filting(path)
+	# ### def __call__
+# ### class _ExcludeFilter
+
 def monitor_configure(config, metastorage):
 	""" 設定監視器組態
 
@@ -53,27 +93,37 @@ def monitor_configure(config, metastorage):
 # ### def monitor_configure
 
 
-class EventHandler(pyinotify.ProcessEvent):
-	def __init__(self, watcher_instance):
+class _EventHandler(pyinotify.ProcessEvent):
+	def __init__(self, watcher_instance, target_directory):
 		pyinotify.ProcessEvent.__init__(self)
 		
 		self.watcher_instance = watcher_instance
+		self.target_directory = target_directory
 	# ### def __init__
+
+	def trigger_operation(self, pathname, watcher_eventcode):
+		path, name, = os.path.split(os.path.abspath(pathname))
+		relpath = _get_relpath(path, self.target_directory)
+		self.watcher_instance.discover_file_change(name, relpath, watcher_eventcode))
+	# ### def perform_modified_operation
 	
 	def process_IN_CLOSE_WRITE(self, event):
-		print "Creating:", event.pathname
+		print "inotify::IN_CLOSE_WRITE: %r" % (event.pathname,)
+		self.trigger_operation(event.pathname, watcher.FEVENT_MODIFIED)
 	# ### def process_IN_CLOSE_WRITE
 
-	def process.IN_MOVED_TO(self, event):
-		print "Moved To:", event.pathname
+	def process_IN_MOVED_TO(self, event):
+		print "inotify::IN_MOVED_TO: %r" % (event.pathname,)
+		self.trigger_operation(event.pathname, watcher.FEVENT_MODIFIED)
 	# ### def process.IN_MOVED_TO
 
 	def process_IN_DELETE(self, event):
-		print "Removing:", event.pathname
+		print "inotify::IN_DELETE: %r" % (event.pathname,)
+		self.trigger_operation(event.pathname, watcher.FEVENT_DELETED)
 	# ### def process_IN_DELETE
 # ### class EventHandler
 
-_wm = None
+_watchmanager = None
 def monitor_start(watcher_instance, target_directory, recursive_watch=False):
 	""" 開始監控目錄作業
 
@@ -85,14 +135,14 @@ def monitor_start(watcher_instance, target_directory, recursive_watch=False):
 		(無)
 	"""
 
-	global _wm
+	global _watchmanager
 
-	_wm = pyinotify.WatchManager()	# Watch Manager
+	_watchmanager = pyinotify.WatchManager(exclude_filter=_ExcludeFilter(target_directory, recursive_watch))
 	mask = pyinotify.IN_DELETE | pyinotify.IN_CLOSE_WRITE | pyinotify.IN_MOVED_TO	# watched events
+	handler = _EventHandler(watcher_instance, target_directory)
 	
-	
-	notifier = pyinotify.AsyncNotifier(_wm, EventHandler(watcher_instance), channel_map=watcher_instance.process_driver.async_map)
-	wdd = _wm.add_watch(target_directory, mask, rec=recursive_watch)
+	notifier = pyinotify.AsyncNotifier(_wm, handler, channel_map=watcher_instance.process_driver.async_map)
+	wdd = _wm.add_watch(target_directory, mask, rec=True)
 # ### def monitor_start
 
 
